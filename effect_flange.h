357533140a1b8ab49553fb80e572451d14b3d8cb
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2023-10-18 11:55:34 +0100

    Prevent crash due to AudioEffectFlange bad initialisation

--- effect_flange.h
+++ effect_flange.h
@@ -38,7 +38,7 @@ public AudioStream
 {
 public:
   AudioEffectFlange(void): 
-	AudioStream(1,inputQueueArray) {}
+	AudioStream(1,inputQueueArray), l_delayline(nullptr) {}
   ~AudioEffectFlange() {SAFE_RELEASE_INPUTS();}
 
   boolean begin(short *delayline,int d_length,int delay_offset,int d_depth,float delay_rate);
