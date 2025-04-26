8a549c355136b183632335262d8a36a6dff1084c
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2025-04-26 08:30:45 +0100

    DEBUG-ONLY commit
    REQUIRES sketch to implement AudioSynthKarplusStrongFail()

--- synth_karplusstrong.cpp
+++ synth_karplusstrong.cpp
@@ -44,10 +44,11 @@ static uint32_t pseudorand(uint32_t lo)
 
 
 uint32_t AudioSynthKarplusStrong::IndexableBuffer::seed = 1;
-bool AudioSynthKarplusStrong::IndexableBuffer::allocate(uint16_t count)
+bool AudioSynthKarplusStrong::IndexableBuffer::allocate(uint16_t count, AudioSynthKarplusStrong* p)
 {
 	bool result = true;
 	size_t i;
+	parent = p;
 	for (i=0; i < count && result; i++)
 	{
 		if (nullptr == buffers[i]) // happens if noteOff() not called - memory leak risk!
@@ -121,7 +122,7 @@ void AudioSynthKarplusStrong::noteOn(float noteFreq, float velocity)
 	state = silent; // in case we haven't had a noteOff(), and an update() occurs
 
 	if (velocity > 1.0f) {
-		velocity = 0.0f;
+		velocity = 0.0f; // BUGBUG: should be 1.0f! leave for now...
 	} else if (velocity <= 0.0f) {
 		noteOff(1.0f);
 		return;
@@ -141,7 +142,7 @@ void AudioSynthKarplusStrong::noteOn(float noteFreq, float velocity)
 	// actual number of samples for requested note
 	baseLen = AUDIO_SAMPLE_RATE_EXACT*increment / noteFreq - increment;
 	
-	if (!theBuffer.allocate(bufferNum)) // couldn't allocate, stay silent
+	if (!theBuffer.allocate(bufferNum, this)) // couldn't allocate, stay silent
 		theBuffer.release();
 	else
 	{
